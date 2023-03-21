d5681140c3db8323813c233be69b05e09332e78d
Blake <102875455+blake502@users.noreply.github.com>
2023-03-21 13:03:51 -0400

    Drawing
    Drawing Sudoku boards of arbitrary size is now possible.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,74 @@
 #include <stdio.h>
+#include <windows.h>
 
 #define root 3
 #define square root*root
+#define spacing 3
 
-int gameboard[square][square] = {
-    {1, 2, 3, 4, 5, 6, 7, 8, 9},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+int gameboard[square*square] = {
+    1, 2, 3, 4, 5, 6, 7, 8, 9,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+static int gameboardIndex = 0;
+char* pieceLetters = " 1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+void print_row(char left_edge[4], char space[4], char content[4], char thin_divider[4], char thick_divider[4], char right_edge[4])
+{
+    printf("%s", left_edge);
+
+        for(int i = 1; i <= root; i++)
+        {
+            for(int j = 1; j <= root; j++)
+            {
+                for(int s = 1; s <= spacing; s++)
+                    if (s!=2)
+                        printf("%s", space);
+                    else
+                        if(content[0] == '!')
+                            printf("%c", pieceLetters[gameboard[gameboardIndex++]]);
+                        else
+                            printf("%s", content);
+                if(j != root)
+                    printf("%s", thin_divider);
+            }
+            if(i == root)
+                printf("%s", right_edge);
+            else
+                printf("%s", thick_divider);
+        }
+        printf("\n");
+}
+
+void print_gameboard()
+{
+    print_row("╔", "═", "═", "╤", "╦", "╗");
+    for(int j = 1; j <= root; j++)
+    {
+        for(int i = 1; i <= root; i++)
+        {
+            print_row("║", " ", "!", "│", "║", "║");
+            if(i!=root)
+                print_row("╟", "─", "─", "┼", "╫", "╢");
+        }
+        if(j!=root)
+            print_row("╠", "═", "═", "╪", "╬", "╣");
+    }
+
+    print_row("╚", "═", "═", "╧", "╩", "╝");
+}
 
 int main()
 {
-    for(int y = 0; y < square; y++)
-    {
-        for(int x = 0; x < square; x++)
-        {
-            printf("%i, ", gameboard[y][x]);
-        }
-        printf("\n");
-    }
+    SetConsoleOutputCP(CP_UTF8);
 
+    print_gameboard();
     
     return 0;
 }
