eb8e1b1f37c2930434fd181c0f10644a412f25e3
Blake <102875455+blake502@users.noreply.github.com>
2023-03-21 14:31:30 -0400

    Building the draw-related functions up

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 #define square root*root
 #define spacing 3
 
+#define clear() printf("\033[H\033[J")
+#define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+
 int gameboard[square*square] = {
     1, 2, 3, 4, 5, 6, 7, 8, 9,
     0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -46,14 +49,14 @@ void print_row(char left_edge[4], char space[4], char content[4], char thin_divi
         printf("\n");
 }
 
-void print_gameboard()
+void draw_gameboard()
 {
     print_row("╔", "═", "═", "╤", "╦", "╗");
     for(int j = 1; j <= root; j++)
     {
         for(int i = 1; i <= root; i++)
         {
-            print_row("║", " ", "!", "│", "║", "║");
+            print_row("║", " ", "?", "│", "║", "║");
             if(i!=root)
                 print_row("╟", "─", "─", "┼", "╫", "╢");
         }
@@ -64,11 +67,31 @@ void print_gameboard()
     print_row("╚", "═", "═", "╧", "╩", "╝");
 }
 
+void fill_number(int x, int y, char c)
+{
+    gotoxy(x * 4 + 3, 2 * y + 2);
+    printf("%c", c);
+}
+
+void fill_numbers()
+{
+    for (int y = 0; y < square; y++)
+        for (int x = 0; x < square; x++)
+            fill_number(x, y, pieceLetters[gameboard[x + y * square]]);
+}
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
+    printf("Press Enter to begin...");
+    getchar();
+    clear();
+    draw_gameboard();
 
-    print_gameboard();
-    
+    for(int i = 0; i < 2300; i++)
+    {
+        fill_numbers();
+    }
+
     return 0;
 }
