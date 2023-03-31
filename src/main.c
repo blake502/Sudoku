8dfd34e2129c0749fcc3d305c747d4239a15b94f
Blake <102875455+blake502@users.noreply.github.com>
2023-03-31 11:54:07 -0400

    Solve Check & Solving
    Completed the Solve Checker, and began implementing Solver

--- src/main.c
+++ src/main.c
@@ -8,6 +8,12 @@
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
 
+typedef struct position2D
+{
+    int x;
+    int y;
+} position2D;
+
 int gameboard[square*square] = {
     1, 4, 7, 2, 8, 6, 3, 5, 9,
     2, 5, 8, 3, 7, 9, 1, 4, 6,
@@ -80,43 +86,124 @@ void fill_numbers()
             fill_number(x, y, pieceLetters[gameboard[x + y * square]]);
 }
 
-_Bool is_solved()
+int* getColContains(int x)
 {
-    //Check columns
-    for(int x = 0; x < square; x++)
-    {
-        int *colContains = malloc(sizeof(int) * square);
+    int *colContains = malloc(sizeof(int) * square);
         ZeroMemory(colContains, sizeof(int) * square);
         for(int y = 0; y < square; y++)
             colContains[gameboard[y * square + x]-1]++;
+    return colContains;
+}
+
+int* getRowContains(int y)
+{
+    int *rowContains = malloc(sizeof(int) * square);
+        ZeroMemory(rowContains, sizeof(int) * square);
+        for(int x = 0; x < square; x++)
+            rowContains[gameboard[y * square + x]-1]++;
+    return rowContains;
+}
+
+int* getBoxContains(int boxx, int boxy)
+{
+    int *boxContains = malloc(sizeof(int) * square);
+            ZeroMemory(boxContains, sizeof(int) * square);
+            for(int x = boxx*root; x < (boxx+1)*root; x++)
+                for(int y = boxy*root; y < (boxy+1)*root; y++)
+                    boxContains[gameboard[y * square + x]-1]++;
+    return boxContains;
+}
+
+_Bool is_solved()
+{
+    _Bool solved = 1;
+    
+    //Check columns
+    for(int x = 0; x < square; x++)
+    {
+        int* colContains = getColContains(x);
     
         for(int i = 0; i < square; i++)
             if (colContains[i] != 1)
             {
-                printf("The puzzle is not solved, because column %i contains %i instances of %c\n", x+1, colContains[i], pieceLetters[i+1]);
-                return 0;
+                printf("Unsolved: Column %i contains %i instances of %c\n", x+1, colContains[i], pieceLetters[i+1]);
+                solved = 0;
             }
     }
 
     //Check rows
     for(int y = 0; y < square; y++)
     {
-        int *rowContains = malloc(sizeof(int) * square);
-        ZeroMemory(rowContains, sizeof(int) * square);
-        for(int x = 0; x < square; x++)
-            rowContains[gameboard[y * square + x]-1]++;
+        int* rowContains = getRowContains(y);
     
         for(int i = 0; i < square; i++)
             if (rowContains[i] != 1)
             {
-                printf("The puzzle is not solved, because column %i contains %i instances of %c\n", y+1, rowContains[i], pieceLetters[i+1]);
-                return 0;
+                printf("Unsolved: Row %i contains %i instances of %c\n", y+1, rowContains[i], pieceLetters[i+1]);
+                solved = 0;
             }
     }
 
-    //TODO: Check boxes
+    //Check boxes
+    for(int boxx = 0; boxx < root; boxx++)
+        for(int boxy = 0; boxy < root; boxy++)
+        {
+            int* boxContains = getBoxContains(boxx, boxy);
 
+            for(int i = 0; i < square; i++)
+                if (boxContains[i] != 1)
+                {
+                    printf("Unsolved: Box (%i, %i) contains %i instances of %c\n", boxx+1, boxy+1, boxContains[i], pieceLetters[i+1]);
+                    solved = 0;
+                } 
+        }
+
+    return solved;
+}
+
+int getEntropy(int x, int y)
+{
+    return -1;
+}
+
+position2D getSpaceWithLowestEntropy()
+{
+    int lowestEntropy = square + 1;
+    position2D position;
+    position.x = -1;
+    position.y = -1;
+    for(int x = 0; x < square; x++)
+        for(int y = 0; y < square; y++)
+            if(gameboard[y*square + x] == 0)
+            {
+                int entropy = getEntropy(x, y);
+                
+                if(entropy < lowestEntropy)
+                {
+                    lowestEntropy = entropy;
+                    position.x = x;
+                    position.y = y;
+                }
+            }
+
+    if(lowestEntropy == 0 || lowestEntropy == square + 1)
+        return position;
+
+    return position;
+}
+
+_Bool makeAMove()
+{
+    position2D lowestEntropy = getSpaceWithLowestEntropy();
+
+    printf("Lowest Entropy: (%i, %i)\n", lowestEntropy.x, lowestEntropy.y);
     return 1;
+}
+
+void solve()
+{
+    makeAMove();
+    //while(!is_solved() && makeAMove()){}
 }
 
 int main()
@@ -126,17 +213,13 @@ int main()
     
     printf("Press Enter to begin...");
     getchar();
-
     clear();
     draw_gameboard();
 
-    is_solved();
+    solve();
 
-    return 0;
-    for(int i = 0; i < 2300; i++)
-    {
-        fill_numbers();
-    }
+    if(is_solved())
+        printf("The puzzle is solved.\n");
 
     return 0;
 }
